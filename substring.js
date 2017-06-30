ad4a39d1f48fb55a1d746190eaa3a30aea1828c7
Robert Avery <daranable@gmail.com>
2017-06-29 17:12:51 -0700

    Switched to slinkysubstring for longest substring match.

--- substring.js
+++ substring.js
@@ -1,32 +1,31 @@
-function longestsubstring (str1, str2) {
-  let subs = {};
-  let sublen = Math.round(str1.length / 2);
-  console.log(sublen);
+function slinkysubstring (str1, str2) {
+  let sub = '';
+  for (var startpos = 0; startpos < str1.length; startpos++) {
+    // if the current sub is larger than the characters left we know we have the
+    // longest substring
+    if (str1.length - startpos < sub.length) break;
+    for (var startpos2 = 0; startpos2 < str2.length; startpos2++) {
+      if (str2.length - startpos2 < sub.length) break;
 
-  while (true) {
-    for (let i = 0; i <= str1.length - sublen; i++) {
-      let sub = str1.substring(i, i + sublen);
-      for (let i = 0; i <= str2.length - sublen; i++) {
-        let sub2 = str2.substring(i, i + sublen);
-        if (sub === sub2) {
-          console.log('MATCH', sub, sub2, sublen);
-          if (!subs[sublen]) subs[sublen] = [];
-          subs[sublen].push(sub);
-        }
+      let length = sub.length + 1;
+      while (str1.substring(startpos, startpos + length) === str2.substring(startpos2, startpos2 + length)) {
+        length++;
+        if (length > str2.length) break;
+      }
+      // We know the loop incremented the length one extra time before it broke
+      // so we decrement it.
+      length--;
+      if (sub.length < length) {
+        sub = str1.substring(startpos, startpos + length);
+        startpos += sub.length;
       }
     }
-    if (!subs[sublen] & typeof subs[sublen - 1] === 'object') {
-      return subs[sublen - 1];
-    } if (!subs[sublen]) {
-      subs[sublen] = -1;
-      sublen--;
-    } else if (typeof subs[sublen] === 'object' & subs[sublen + 1] === -1) {
-      return subs[sublen];
-    } else {
-      sublen++;
-    }
   }
+  return sub;
 }
 
-console.log('Longest Substring: ' + longestsubstring('catapults', 'ultrasound'));
-console.log('Longest Substring: ' + longestsubstring('dignified', 'unified'));
+console.log('Longest Substring: ' + slinkysubstring('ultrulta', 'sultiulta'));
+console.log('Longest Substring: ' + slinkysubstring('dignified', 'unified'));
+console.log('Longest Substring: ' + slinkysubstring('unified', 'unified'));
+console.log('Longest Substring: ' + slinkysubstring('mat', 'sring'));
+console.log('Longest Substring: ' + slinkysubstring('swarray', 'array'));
